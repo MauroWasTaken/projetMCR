92bd968ca46617fd7b0146cd1ac1d917e2132f8a
mauro <maurx18@gmail.com>
2026-06-14 16:41:00 +0200

    fixes weird text rendering in upgrade menu

--- core/src/main/java/bullethell/states/UpgradeMenuState.java
+++ core/src/main/java/bullethell/states/UpgradeMenuState.java
@@ -145,7 +145,7 @@ public class UpgradeMenuState extends AbstractGameState {
     public void render() {
         final float playHeight = context.getPlayHeight();
         this.writer.writeCenteredTextAtHeight("UPGRADE MENU", playHeight - 50, 1.5f);
-        this.writer.writeTightMultilineAtHeight(new String[]{"Use numbers 1-9 to pick your weapon", "Press ENTER to start level"}, playHeight - 80, 1f);
+        this.writer.writeTightMultilineAtHeight(new String[]{"Use numbers 1-9 to pick your weapon"," ", "Press ENTER to start level"}, playHeight - 80, 1f);
 
         float startY = playHeight - 120;
 
